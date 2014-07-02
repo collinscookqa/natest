db5cbf27205b8220bdc7f6adbaaab32b9a21bf08
collinscookqa <collins.obasuyi@thomascookonline.com>
2014-07-02 15:59:18 +0100

    test

--- src/main/java/firstproject/natclass.java
+++ src/main/java/firstproject/natclass.java
@@ -4,15 +4,16 @@ package firstproject;
 
 public class natclass {
 	
+	int first_numbe
 	
-	public static void main(String[ ] args) {
-
-		int first_numbe;
-
-		first_number = 10;
-
-		System.out.println( "First number = " + first_number );
-
-		}
+//	public static void main(String[ ] args) {
+//
+//		int first_numbe;
+//
+//		first_number = 10;
+//
+//		System.out.println( "First number = " + first_number );
+//
+//		}
 
 }
