0ee5dea08cdeb3c1d7ad56ca139f409993e5444f
collinscookqa <collins.obasuyi@thomascookonline.com>
2014-07-02 15:17:40 +0100

    init commit

---
+++ src/firstproject/natclass.java
@@ -0,0 +1,18 @@
+
+
+package firstproject; 
+
+public class natclass {
+	
+	
+	public static void main(String[ ] args) {
+
+		int first_number;
+
+		first_number = 10;
+
+		System.out.println( "First number = " + first_number );
+
+		}
+
+}
